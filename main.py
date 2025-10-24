9fd4775046bea58be8e8e29eb6cf74ee2d593ab3
sunyating1 <1613152158@qq.com>
2025-10-24 13:50:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def PrimeList(N):
-    primes = []
-    for num in range(2, N):
-        is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(str(num))
-    print(' '.join(primes))
+def primelist(n):
+    is_prime = [True] * n
+    if n > 0:
+        is_prime[0] = False
+    if n > 1:
+        is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n, i):
+                is_prime[j] = False
+    primes = [i for i, prime in enumerate(is_prime) if prime]
+    return primes
+n = int(input())
+result = primelist(n)
+print(' '.join(map(str, result)))
 
